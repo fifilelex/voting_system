5a223cdbd55c5d9b3d5f7f4e474864febb5a1d9e
fifilelex <fifilelex@gmail.com>
2025-12-04 20:24:02 +0100

    add comments in MenuManager.py

--- src/io/MenuManager.py
+++ src/io/MenuManager.py
@@ -5,45 +5,45 @@ from src.models.voter import *
 def call_submenu_handler(io, election):
     call_submenu()
     match io.getInput():
-                case "1":
+                case "1": #add candidate
                     name = io.getCandidateName_add()
                     candid = Candidate(name)
                     election.add_candidate(candid)
-                case "2":
+                case "2": #edit candidate
                     current_name, new_name = io.getCandidateName_edit()
                     if election.edit_candidate(current_name, new_name):
                         io.success()
                     else:
                         io.error()
-                case "3":
+                case "3": #delete candidate
                     name = io.getCandidateName_remove()
                     if election.delete_candidate(name):
                         io.success()
                     else:
                         io.error()
-                case "4":
+                case "4": #get list of all candidates
                     election.getCandidates()
-                case "5":
+                case "5": #return to main menu
                     call_menu_handler(io, election)
                 case _:
                     io.error()
 def call_menu_handler(io, election):
     call_menu()
     match io.getInput():
-        case "0":
+        case "0": #get list of all candidates
             election.getCandidates()
-        case "1":
+        case "1": #show menu for candidates management
             call_submenu()
             call_submenu_handler(io, election)
-        case "2":
+        case "2": #vote for x
             voter_name, candid_name = io.getVotingData()
 
             election.vote(voter_name, candid_name)
-        case "3":
+        case "3": #get election result
             election.results()
-        case "4":
+        case "4": #close app
             election.close()
             return False
-        case _:
+        case _: #wrong choice
             io.error()
     return True
