97e5c08f6e1dc3efae32439cfd9320953067b50a
fifilelex <fifilelex@gmail.com>
2025-12-04 19:50:02 +0100

    Refactor: move menu handling from main.py to MenuManager.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src.io.io_manager import *
-from src.io.menu import *
-from src.models.candidate import Candidate
+from src.io.MenuManager import *
 from src.services.elections import Election
 from tests.test import sample_test_data
 
@@ -9,49 +8,9 @@ from tests.test import sample_test_data
 election = Election("wybory prezydenckie", 2045)
 io = IOManager()
 sample_test_data(election)
+running = True
+while running:
+    running = call_menu_handler(io, election)
 
-while True:
-    call_menu()
 
-    match io.getInput():
-        case "0":
-            election.getCandidates()
-        case "1":
-            call_submenu()
-            match io.getInput():
 
-                case "1":
-                    name = io.getCandidateName_add()
-                    candid = Candidate(name)
-                    election.add_candidate(candid)
-
-                case "2":
-                    current_name, new_name = io.getCandidateName_edit()
-                    if election.edit_candidate(current_name, new_name):
-                        io.success()
-                    else:
-                        io.error()
-
-                case "3":
-                    name = io.getCandidateName_remove()
-                    if election.delete_candidate(name):
-                        io.success()
-                    else:
-                        io.error()
-                case "4":
-                    election.getCandidates()
-                case "5":
-                    break
-                case _:
-                    io.error()
-        case "2":
-            voter_name, candid_name = io.getVotingData()
-
-            election.vote(voter_name, candid_name)
-        case "3":
-            election.results()
-        case "4":
-            election.close()
-            break
-        case _:
-            io.error()
--- src/io/MenuManager.py
+++ src/io/MenuManager.py
@@ -1,3 +1,49 @@
+from src.io.menu import *
 from src.models.candidate import *
 from src.models.voter import *
 
+def call_submenu_handler(io, election):
+    call_submenu()
+    match io.getInput():
+                case "1":
+                    name = io.getCandidateName_add()
+                    candid = Candidate(name)
+                    election.add_candidate(candid)
+                case "2":
+                    current_name, new_name = io.getCandidateName_edit()
+                    if election.edit_candidate(current_name, new_name):
+                        io.success()
+                    else:
+                        io.error()
+                case "3":
+                    name = io.getCandidateName_remove()
+                    if election.delete_candidate(name):
+                        io.success()
+                    else:
+                        io.error()
+                case "4":
+                    election.getCandidates()
+                case "5":
+                    call_menu_handler(io, election)
+                case _:
+                    io.error()
+def call_menu_handler(io, election):
+    call_menu()
+    match io.getInput():
+        case "0":
+            election.getCandidates()
+        case "1":
+            call_submenu()
+            call_submenu_handler(io, election)
+        case "2":
+            voter_name, candid_name = io.getVotingData()
+
+            election.vote(voter_name, candid_name)
+        case "3":
+            election.results()
+        case "4":
+            election.close()
+            return False
+        case _:
+            io.error()
+    return True
