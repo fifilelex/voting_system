77822bf73b42b5dae9b6ad496fb13b69fc6a0f3b
fifilelex <fifilelex@gmail.com>
2025-12-09 14:15:41 +0100

    Add: add_vote test overhaul

--- tests/test_candidate.py
+++ tests/test_candidate.py
@@ -1,6 +1,24 @@
-
+import random
 from src.models.candidate import Candidate
+def candidate():
+    return Candidate("Tytus Ciezki")
 def test_add_vote():
-    c = Candidate("Andrzej Duda")
-    c.add_vote()
-    assert Candidate.get_votes(c) == 1
+
+    c = candidate()
+
+    for i in range(1, 4):
+        c.add_vote()
+        assert c.get_votes() == i
+
+    for _ in range(3):
+        c.add_vote()
+
+    assert c.get_votes() == 6
+
+    expected_votes = 6
+    for i in range(7, 13):
+        c.add_vote()
+        expected_votes += 1
+        assert c.get_votes() == expected_votes
+
+    c.reset_votes()
