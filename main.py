8706e8dd8cc1c0a62b8a733b0c74c93a97df11fd
fifilelex <fifilelex@gmail.com>
2025-12-11 01:38:47 +0100

    Refactor: main.py logic fix, app loop should work better

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from src.io.io_manager import *
+from src.io.io_manager import IOManager
 from src.io.MenuManager import *
 from src.services.elections import Election
 from tests.test import sample_test_data
 
 
 #MENU
-election = Election("wybory prezydenckie", 2045)
-io = IOManager()
-sample_test_data(election)
-running = True
-while running:
-    running = call_menu_handler(io, election)
+
+def main():
+    election = Election("wybory prezydenckie", 2045)
+    io = IOManager()
+    sample_test_data(election)
+    running = True
+    subrunning = False
+    while running:
+        action = call_menu_handler(io, election)
+        if action == "next":
+            subrunning = True
+            while subrunning:
+                subaction = call_submenu_handler(io, election)
+                if subaction == "back":
+                    subrunning = False
+                else:
+                    pass
+        else:
+            pass
+
+
+main()
+
+
+
--- src/io/MenuManager.py
+++ src/io/MenuManager.py
@@ -1,5 +1,5 @@
 from src.io.menu import *
-from src.models.candidate import *
+from src.models.candidate import Candidate
 
 def call_submenu_handler(io, election):
 
@@ -17,26 +17,27 @@ def call_submenu_handler(io, election):
     elif choice == 5: #return to main menu
         return "back"
     else:
-        io.error()
+        return False
 def call_menu_handler(io, election):
-    match io.getInput():
-        case "0": #get list of all candidates
-            election.getCandidates()
-        case "1": #show menu for candidates management
-            call_submenu()
-            call_submenu_handler(io, election)
-        case "2": #vote for x
+        choice = int(io.getInput)
+        if choice ==  0: #get list of all candidates
+                election.getCandidates()
+                return True
+        elif choice == 1: #show menu for candidates management
+                return "next"
+        elif choice == 2: #vote for x
             voter_name, candid_name = io.getVotingData()
-
             if election.vote(voter_name, candid_name):
-                io.success()
+                    io.success()
+                    return True
             else:
                 io.error()
-        case "3": #get election result
-            election.results()
-        case "4": #close app
-            election.close()
-            return False
-        case _: #wrong choice
+                return False
+        elif choice == 3: #get election result
+                election.results()
+        elif choice == 4: #close app
+                election.close()
+                return False
+        else: #wrong choice
             io.error()
-    return True
+            return False
