ff70352148c452f53c0b0dba1add616c1420487a
fifilelex <fifilelex@gmail.com>
2025-12-14 10:35:22 +0100

    bugfix: sample_test_data now adds Candidate properly
    previously it tried to pass Candidate("name") into method add_candidate
    resulting in:
    expected: new_candidate = Candidate(name)
    given: new_candidate = Candidate(Candidate(name))

--- tests/test.py
+++ tests/test.py
@@ -2,10 +2,10 @@ from src.models.candidate import Candidate
 from src.models.voter import Voter
 
 def sample_test_data(election): #used as sample data, to make testing faster
-    election.add_candidate(Candidate("Grzegorz Braun"))
-    election.add_candidate(Candidate("Adrian Zandberg"))
-    election.add_candidate(Candidate("Amine Papiński"))
-    election.add_candidate(Candidate("Jarosław Staszkiewicz"))
+    election.add_candidate("Grzegorz Braun")
+    election.add_candidate("Adrian Zandberg")
+    election.add_candidate("Amine Papiński")
+    election.add_candidate("Jarosław Staszkiewicz")
     election.add_voter("Adenoid Hynkiel", 1)
     election.add_voter("Tomasz Perła", 2)
     election.add_voter("Zbigniew Kucharski", 1)
