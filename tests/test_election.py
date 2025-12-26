79f858171479db37473968a9384f025c701467a3
fifilelex <fifilelex@gmail.com>
2025-12-26 17:25:13 +0100

    fix: add voter func now checks whether add_voter returns true value
    previously test checked only effect of add_voter (without checking whether function returns expected value)

--- tests/test_election.py
+++ tests/test_election.py
@@ -27,38 +27,17 @@ def test_add_voter(election_with_data, input_x, input_y, expected):
 
     assert e.add_voter(input_x, input_y) == expected
 
-    name, nr = "Orzeł Ówtąrzszny", 2
-    assert e.add_voter(name, nr) is True
-    assert e.isVoter(name, nr), f"Voter {name} with nr {nr} not found"
-def test_add_n_voters():
-    e = election()
+
+def test_add_n_voters(election_with_data):
+    e = election_with_data
     new_voters = [
     ["Anna Kowalska", 1],
-    ["Piotr Nowak", 2],
-    ["Katarzyna Wiśniewska", 3],
-    ["Marcin Wójcik", 4],
-    ["Agnieszka Kamińska", 5],
-    ["Tomasz Lewandowski", 6],
-    ["Magdalena Zielińska", 7],
-    ["Paweł Szymański", 8],
-    ["Joanna Kaczmarek", 9],
-    ["Marek Jabłoński", 10],
-    ["Ewa Mazur", 11],
-    ["Łukasz Wojciechowski", 12],
-    ["Monika Piotrowska", 13],
-    ["Krzysztof Grabowski", 14],
-    ["Natalia Pawłowska", 15],
-    ["Adam Górski", 16],
-    ["Barbara Dudek", 17],
-    ["Grzegorz Rutkowski", 18],
-    ["Sylwia Michalska", 19],
-    ["Rafał Nowicki", 20]
-]
+    ["Piotr Nowak", 2]]
 
     for i in range(len(new_voters)):
         name = new_voters[i][0]
         nr = new_voters[i][1]
-        e.add_voter(name, nr)
+        assert e.add_voter(name, nr)
         assert e.isVoter(name, nr), f"Voter {name} with nr {nr} not found"
 
 
