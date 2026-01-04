ec22b1e36e5c6c781e8cfd26af295fd726e77319
fifilelex <fifilelex@gmail.com>
2026-01-04 11:54:45 +0100

    refactor rename getCandidates to get_candidates

--- src/models/candidate.py
+++ src/models/candidate.py
@@ -13,7 +13,7 @@ class Candidate:
         self.votes = 0
     def to_dict(self):
         return {"name": self.name, "votes": self.votes}
-    def getCandidates(self):
+    def get_candidates(self):
         return self.name
     def __str__(self):
         return f"{self.name} - Votes: {self.votes}" #used to return each candidate name and his result
